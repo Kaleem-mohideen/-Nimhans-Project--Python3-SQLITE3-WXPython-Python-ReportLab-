c4b55d486e82687bd13bd1a80749af7f95f0258f
Vivek Radhakrishnan <vivek@cpcdiagnostics.in>
2020-09-30 13:47:48 +0530

    added code for email to utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import os
 from pathlib import Path
 import datetime
 import re
+import smtplib
+import json
+import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import mimetypes
+import base64
+
+
+
+
 
 def createDir():
     _today = datetime.datetime.now()
@@ -36,6 +48,66 @@ def getAge(_dob):
     _diff = _today.year - _dob.year - ((_today.month, _today.day) < (_dob.month, _dob.day))
     return '{0} years'.format(_diff)
 
+
+
+def sendEmail(_mailDict, _creds=None):
+    '''
+    _mailDict { key :
+                {
+                    'to': [_email...],
+                    'cc': [_email....],
+                    'bcc' = [_email...],
+                    'body': _body,
+                    'subject': _subject,
+                    'attachments' : [list of attachments]
+                }
+              }
+    _creds = {'from':_fromAddress, 'reply-to': if not the same as from address, 'login':, 'password':} - if none - reads from a file
+    '''
+    #IMPORTS HERE TO BE MOVED TO APPROPRIATE FILE
+    if _creds == None:
+        with open ('instance/creds.json') as f:
+            _creds = json.load(f)
+    print (_creds)
+    server =  smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    print(server.login(_creds['login'], _creds['auth']))
+    print(_mailDict)
+    for _key in _mailDict:
+        print('_key: ', _key)
+        if _mailDict[_key]['body'] == '':
+            print("_mainDict[_key]['body']: ", _mainDict[_key]['body'])
+            continue
+        print('_key: ', _key)
+        _senders =_mailDict[_key]['to']
+        print('_senders: ', _senders)
+        msg = MIMEMultipart()
+        msg['From'] = _creds['login']
+        if 'from' in _creds:
+            msg['From'] = _creds['from']
+        if 'reply-to' in _creds:
+            msg['reply-to'] = _creds['reply-to']
+        msg['To'] = ', '.join(_mailDict[_key]['to'])
+        if 'cc' in _mailDict[_key]:
+            _senders+= _mailDict[_key]['cc']
+            msg['CC'] = ', '.join(_mailDict[_key]['cc'])
+        if 'bcc' in _mailDict[_key]:
+            _senders+= _mailDict[_key]['bcc']
+        msg['Subject'] = _mailDict[_key]['subject']
+        msg.attach(MIMEText(_mailDict[_key]['body'],'html'))
+        if 'attachments' in _mailDict[_key]:
+            for _filename in _mailDict[_key]['attachments']:
+                try:
+                    print(_filename, '   attaching...')
+                    _file = open(_filename, 'rb')
+                    _part= MIMEApplication(_file.read(),Name=os.path.basename(_filename))
+                    msg.attach(_part)
+                    _file.close()
+                except Exception as ex:
+                    print (ex)
+        print(server.sendmail(_creds['login'], _senders , msg.as_string()))
+    server.quit()
 
 
 if __name__ == '__main__':
